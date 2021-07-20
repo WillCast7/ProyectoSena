cbdfa8d9184727c2921464e78877eaa1c6d58c31
MariaAlejandraBoadaRodriguez <alejandrarodriguez2508@gmail.com>
2021-07-20 11:41:46 -0500

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,25 @@
 @extends('dashboard.base')
 @section('contenido')
-<h1>sdsdsdsf</h1>
+<div>
+    <ul>
+        <div id="Marco">
+            <div id="Listado">
+                <div id="imagen-listado"><i class="fas fa-clipboard-list"></i></div>
+                <span id="texto-listado">Listado</span>
+            </div>
+            <div id="Marcas">
+                <div id="imagen-marcas"><i class="far fa-registered"></i></div>
+                <span id="texto-marcas">Marcas</span>
+            </div>
+            <div id="Categorias">
+                <div id="imagen-categorias"><i class="fas fa-angle-double-down"></i></div>
+                <span id="texto-categorias">Categorias</span>
+            </div>
+            <div id="Eccomerce">
+                <div id="imagen-eccomerce"><i class="far fa-money-bill-alt"></i></div>
+                <span id="texto-eccomerce">Eccomerce</span>
+            </div>
+        </div>
+    </ul>
+</div>
 @endsection
